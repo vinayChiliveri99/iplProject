6a7e9bd3d0d1ffb7ea87ea29aa01335de3f73361
vinaychiliverii <vinaychiliveri99@gmail.com>
2023-11-16 09:27:21 +0530

    .DS_Store files deleted

---
+++ src/server/7-strike-rate-batsman-each-season.js
@@ -0,0 +1,5 @@
+// Find the strike rate of a batsman for each season.
+
+// for every season extract match ids,
+// for that particular season, iterate over deliveries using match ids, and then,
+// add those batsman to a object and keep on updating strike rate (directly add runs for every ball and divide runs/ball * 100).
