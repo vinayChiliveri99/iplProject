4154e24a1bf74f218ec98d463aa7ebf1ef84ffe3
vinaychiliverii <vinaychiliveri99@gmail.com>
2023-11-18 21:59:29 +0530

    team won both toss and match, refactored

--- src/server/5-team-won-toss-match.js
+++ src/server/5-team-won-toss-match.js
@@ -1,7 +1,12 @@
-// Find the number of times each team won the toss and also won the match
+// 5. Find the number of times each team won the toss and also won the match
+
+// this is the structure of output, we aiming at.
+// check if the result has this match winning team, if so upate it.
+// if no, then create one and assign its value to be 1.
+// added a edge case, to avoid errors, when the match winner and toss winner data is absent
 
 /*
-    object = {
+    result = {
         team1 : count++,
         team2 : count++;
     }
@@ -9,7 +14,11 @@
 
 function countWinOfTossAndMatchWithHOF(matches) {
   return matches.reduce((acc, match) => {
-    if (match.toss_winner === match.winner) {
+    if (
+      match.toss_winner === match.winner &&
+      match.toss_winner !== '' &&
+      match.winner !== ''
+    ) {
       if (acc[match.winner]) {
         acc[match.winner] += 1;
       } else {
