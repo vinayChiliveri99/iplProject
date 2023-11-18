dfa14e984570e8bf45321418e651dd19a947b2cf
vinaychiliverii <vinaychiliveri99@gmail.com>
2023-11-18 22:44:49 +0530

    extra runs conceaded per team in 2015, refactored

--- src/server/3-extra-runs-per-team-2016.js
+++ src/server/3-extra-runs-per-team-2016.js
@@ -1,10 +1,10 @@
 //3. Extra runs conceded per team in the year 2016
 
 // the common thing in both data is match id.
-// get the match ids from the year 2016.
-// and then for every match just calculate the extra runs (by bowling team) in innings 1 and 2 respectively.
 
-// the output can be like this
+// and then for every match just calculate the extra runs (by bowling team).
+
+// the output has to be like this
 /*
 {
 rcb : 10,
@@ -15,6 +15,8 @@ rr : 20,
 */
 
 function extraRunsCalculatorUsingHOF(matches, deliveries) {
+  // get the match ids from the year 2016.
+
   const matchIds = matches.reduce((acc, match) => {
     if (match.season === '2016') {
       acc.add(match.id);
@@ -27,13 +29,17 @@ function extraRunsCalculatorUsingHOF(matches, deliveries) {
   const result = deliveries.reduce((acc, delivery) => {
     let currentMatchId = delivery.match_id;
     let team = delivery.bowling_team;
-    let extraRuns = delivery.extra_runs;
+    let extraRuns = parseInt(delivery.extra_runs);
 
-    if (matchIds.has(currentMatchId)) {
+    // checking if current match id, is part of the match id's set we have, and the team is not empty.
+    // if so keep updating the acc, if it has team by adding extra runs.
+    // if not, just create one and assign extra runs to it.
+
+    if (matchIds.has(currentMatchId) && team !== '') {
       if (acc[team]) {
-        acc[team] = parseInt(acc[team]) + parseInt(extraRuns);
+        acc[team] = acc[team] + extraRuns;
       } else {
-        acc[team] = parseInt(extraRuns);
+        acc[team] = extraRuns;
       }
     }
 
