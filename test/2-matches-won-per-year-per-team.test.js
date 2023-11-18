4682e004a1076b3756548f6cfbcee5e6ab550246
vinaychiliverii <vinaychiliveri99@gmail.com>
2023-11-18 18:27:16 +0530

    test cases for matches won per team, per year added

--- test/2-matches-won-per-year-per-team.test.js
+++ test/2-matches-won-per-year-per-team.test.js
@@ -1,16 +1,257 @@
 const findNoOfMatchesWonPerTeamPerYearWithHOF = require('../src/server/2-matches-won-per-year-per-team');
 
+// 1. with empty input.
+
 test('count no.of matches won per team per team with empty', () => {
   expect(findNoOfMatchesWonPerTeamPerYearWithHOF([])).toStrictEqual(
     {}
   );
 });
 
+// 2. with full data.
+
 const matches = require('../src/data/matchesInJson.json');
-const output1 = require('../src/public/output/2-matchesWonPerYearPerTeam.json');
+const output2 = require('../src/public/output/2-matchesWonPerYearPerTeam.json');
 
 test('count no.of matches won per team per year with full data', () => {
   expect(
     findNoOfMatchesWonPerTeamPerYearWithHOF(matches)
-  ).toStrictEqual(output1);
+  ).toStrictEqual(output2);
+});
+
+// 3. data where the match winner is not present, the output should skip when the winner is empty.
+
+const testFile3 = [
+  {
+    id: '1',
+    season: '2017',
+    city: 'Hyderabad',
+    date: '2017-04-05',
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+    toss_winner: 'Royal Challengers Bangalore',
+    toss_decision: 'field',
+    result: 'normal',
+    dl_applied: '0',
+    winner: '',
+    win_by_runs: '35',
+    win_by_wickets: '0',
+    player_of_match: 'Yuvraj Singh',
+    venue: 'Rajiv Gandhi International Stadium, Uppal',
+    umpire1: 'AY Dandekar',
+    umpire2: 'NJ Llong',
+    umpire3: '',
+  },
+  {
+    id: '2',
+    season: '2017',
+    city: 'Hyderabad',
+    date: '2017-04-05',
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+    toss_winner: 'Royal Challengers Bangalore',
+    toss_decision: 'field',
+    result: 'normal',
+    dl_applied: '0',
+    winner: 'Sunrisers Hyderabad',
+    win_by_runs: '35',
+    win_by_wickets: '0',
+    player_of_match: 'Yuvraj Singh',
+    venue: 'Rajiv Gandhi International Stadium, Uppal',
+    umpire1: 'AY Dandekar',
+    umpire2: 'NJ Llong',
+    umpire3: '',
+  },
+  {
+    id: '3',
+    season: '2018',
+    city: 'Hyderabad',
+    date: '2017-04-05',
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+    toss_winner: 'Royal Challengers Bangalore',
+    toss_decision: 'field',
+    result: 'normal',
+    dl_applied: '0',
+    winner: 'Royal Challengers Bangalore',
+    win_by_runs: '35',
+    win_by_wickets: '0',
+    player_of_match: 'Yuvraj Singh',
+    venue: 'Rajiv Gandhi International Stadium, Uppal',
+    umpire1: 'AY Dandekar',
+    umpire2: 'NJ Llong',
+    umpire3: '',
+  },
+];
+
+let output3 = {
+  2017: {
+    'Sunrisers Hyderabad': 1,
+  },
+  2018: {
+    'Royal Challengers Bangalore': 1,
+  },
+};
+
+test('count no.of matches won per team per year when match winner is empty', () => {
+  expect(
+    findNoOfMatchesWonPerTeamPerYearWithHOF(testFile3)
+  ).toStrictEqual(output3);
+});
+
+// 4. when year is not defined in the data.
+
+const testFile4 = [
+  {
+    id: '1',
+    season: '2015',
+    city: 'Hyderabad',
+    date: '2017-04-05',
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+    toss_winner: 'Royal Challengers Bangalore',
+    toss_decision: 'field',
+    result: 'normal',
+    dl_applied: '0',
+    winner: 'Sunrisers Hyderabad',
+    win_by_runs: '35',
+    win_by_wickets: '0',
+    player_of_match: 'Yuvraj Singh',
+    venue: 'Rajiv Gandhi International Stadium, Uppal',
+    umpire1: 'AY Dandekar',
+    umpire2: 'NJ Llong',
+    umpire3: '',
+  },
+  {
+    id: '2',
+    season: '',
+    city: 'Hyderabad',
+    date: '2017-04-05',
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+    toss_winner: 'Royal Challengers Bangalore',
+    toss_decision: 'field',
+    result: 'normal',
+    dl_applied: '0',
+    winner: 'Sunrisers Hyderabad',
+    win_by_runs: '35',
+    win_by_wickets: '0',
+    player_of_match: 'Yuvraj Singh',
+    venue: 'Rajiv Gandhi International Stadium, Uppal',
+    umpire1: 'AY Dandekar',
+    umpire2: 'NJ Llong',
+    umpire3: '',
+  },
+  {
+    id: '3',
+    season: '2018',
+    city: 'Hyderabad',
+    date: '2017-04-05',
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+    toss_winner: 'Royal Challengers Bangalore',
+    toss_decision: 'field',
+    result: 'normal',
+    dl_applied: '0',
+    winner: 'Royal Challengers Bangalore',
+    win_by_runs: '35',
+    win_by_wickets: '0',
+    player_of_match: 'Yuvraj Singh',
+    venue: 'Rajiv Gandhi International Stadium, Uppal',
+    umpire1: 'AY Dandekar',
+    umpire2: 'NJ Llong',
+    umpire3: '',
+  },
+];
+
+let output4 = {
+  2015: {
+    'Sunrisers Hyderabad': 1,
+  },
+  2018: {
+    'Royal Challengers Bangalore': 1,
+  },
+};
+
+test('count no.of matches won per team per year when year is empty', () => {
+  expect(
+    findNoOfMatchesWonPerTeamPerYearWithHOF(testFile4)
+  ).toStrictEqual(output4);
+});
+
+// 5. when both year and match winner is not defined in the data.
+
+const testFile5 = [
+  {
+    id: '1',
+    season: '2015',
+    city: 'Hyderabad',
+    date: '2017-04-05',
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+    toss_winner: 'Royal Challengers Bangalore',
+    toss_decision: 'field',
+    result: 'normal',
+    dl_applied: '0',
+    winner: '',
+    win_by_runs: '35',
+    win_by_wickets: '0',
+    player_of_match: 'Yuvraj Singh',
+    venue: 'Rajiv Gandhi International Stadium, Uppal',
+    umpire1: 'AY Dandekar',
+    umpire2: 'NJ Llong',
+    umpire3: '',
+  },
+  {
+    id: '2',
+    season: '',
+    city: 'Hyderabad',
+    date: '2017-04-05',
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+    toss_winner: 'Royal Challengers Bangalore',
+    toss_decision: 'field',
+    result: 'normal',
+    dl_applied: '0',
+    winner: 'Sunrisers Hyderabad',
+    win_by_runs: '35',
+    win_by_wickets: '0',
+    player_of_match: 'Yuvraj Singh',
+    venue: 'Rajiv Gandhi International Stadium, Uppal',
+    umpire1: 'AY Dandekar',
+    umpire2: 'NJ Llong',
+    umpire3: '',
+  },
+  {
+    id: '3',
+    season: '2018',
+    city: 'Hyderabad',
+    date: '2017-04-05',
+    team1: 'Sunrisers Hyderabad',
+    team2: 'Royal Challengers Bangalore',
+    toss_winner: 'Royal Challengers Bangalore',
+    toss_decision: 'field',
+    result: 'normal',
+    dl_applied: '0',
+    winner: 'Royal Challengers Bangalore',
+    win_by_runs: '35',
+    win_by_wickets: '0',
+    player_of_match: 'Yuvraj Singh',
+    venue: 'Rajiv Gandhi International Stadium, Uppal',
+    umpire1: 'AY Dandekar',
+    umpire2: 'NJ Llong',
+    umpire3: '',
+  },
+];
+
+let output5 = {
+  2018: {
+    'Royal Challengers Bangalore': 1,
+  },
+};
+
+test('count no.of matches won per team per year when year and match winner is empty', () => {
+  expect(
+    findNoOfMatchesWonPerTeamPerYearWithHOF(testFile5)
+  ).toStrictEqual(output5);
 });
