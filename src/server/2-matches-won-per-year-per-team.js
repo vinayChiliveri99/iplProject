238e058db92c2d4d7d92d14e990d0cfbbd89a5fd
vinaychiliverii <vinaychiliveri99@gmail.com>
2023-11-19 10:11:32 +0530

    matches won per year per team,  refactored

--- src/server/2-matches-won-per-year-per-team.js
+++ src/server/2-matches-won-per-year-per-team.js
@@ -1,11 +1,17 @@
 // 2. Number of matches won per team per year in IPL.
 
 function findNoOfMatchesWonPerTeamPerYearWithHOF(matches) {
-  let result = matches.reduce((acc, match) => {
+  // checking if the year is not empty and winner team is not empty, to avoid errors.
+
+  return matches.reduce((acc, match) => {
     let winnerTeam = match.winner;
     let year = match.season;
 
     if (winnerTeam !== '' && year !== '') {
+      // when the data is clear, move ahead with creating the object,
+      // if there's no object with the winning team in that year, create one
+      // if its already present just keep updating the values.
+
       if (acc[year] !== undefined) {
         if (acc[year][winnerTeam] !== undefined) {
           acc[year][winnerTeam] += 1;
@@ -19,8 +25,23 @@ function findNoOfMatchesWonPerTeamPerYearWithHOF(matches) {
 
     return acc;
   }, {});
-
-  return result;
 }
 
 module.exports = findNoOfMatchesWonPerTeamPerYearWithHOF;
+
+/*
+  By Object Destructuring :
+
+  function findNoOfMatchesWonPerTeamPerYearWithHOF(matches) {
+  return matches.reduce((acc, { winner, season }) => {
+    if (winner && season) {
+      acc[season] = acc[season] || {};
+      acc[season][winner] = (acc[season][winner] || 0) + 1;
+    }
+    return acc;
+  }, {});
+}
+
+module.exports = findNoOfMatchesWonPerTeamPerYearWithHOF;
+
+*/
