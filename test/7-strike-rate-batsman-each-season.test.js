5f7d43dde31ef6b45bcbe62a7f342f3b7bf212a7
vinaychiliverii <vinaychiliveri99@gmail.com>
2023-11-20 09:59:59 +0530

    test cases for strike rate a batsman, added

--- test/7-strike-rate-batsman-each-season.test.js
+++ test/7-strike-rate-batsman-each-season.test.js
@@ -3,7 +3,7 @@ const deliveries = require('../src/data/deliveriesInJson.json');
 
 const strikeRateOfaBatsmanForEachSeasonUSingHOF = require('../src/server/7-strike-rate-batsman-each-season');
 
-// with empty data
+// 1. with empty data
 
 test('strike of a batsman for every season with empty data', () => {
   expect(
@@ -11,8 +11,8 @@ test('strike of a batsman for every season with empty data', () => {
   ).toStrictEqual({});
 });
 
-// with full data.
-const output1 = require('../src/public/output/7-strikeRateOfaBatsmanForEachSeason.json');
+// 2. with full data.
+const output2 = require('../src/public/output/7-strikeRateOfaBatsmanForEachSeason.json');
 
 test('strike rate of a batsman (MS Dhoni) for every season with full data', () => {
   expect(
@@ -21,5 +21,29 @@ test('strike rate of a batsman (MS Dhoni) for every season with full data', () =
       deliveries,
       'MS Dhoni'
     )
-  ).toStrictEqual(output1);
+  ).toStrictEqual(output2);
+});
+
+// 3. to match with some other players data.
+
+const output3 = {
+  2009: 123.48,
+  2010: 146.03,
+  2011: 117.82,
+  2012: 162.58,
+  2013: 126.54,
+  2014: 142.32,
+  2015: 155.62,
+  2016: 151.25,
+  2017: 141.2,
+};
+
+test('strike rate of DA Warner is being tested, with full data', () => {
+  expect(
+    strikeRateOfaBatsmanForEachSeasonUSingHOF(
+      matches,
+      deliveries,
+      'DA Warner'
+    )
+  ).toStrictEqual(output3);
 });
